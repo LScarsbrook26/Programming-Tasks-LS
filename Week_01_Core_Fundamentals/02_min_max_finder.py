c86c2f1dae5a211b1b33e2f638bd24d90239d011
LScarsbrook26 <48038@rws.uk.net>
2026-09-17 18:09:56 +0100

    02_min_max_finder.py
    Added functionality to find minimum and maximum values from user input.

--- Week_01_Core_Fundamentals/02_min_max_finder.py
+++ Week_01_Core_Fundamentals/02_min_max_finder.py
@@ -11,13 +11,30 @@ TODO:
 - Fill in functions
 - Add demonstration code under `if __name__ == "__main__":`
 """
+def find_min_max():
+    numbers = []
+    add = True
+    while add == True:
+        num = int(input(" enter number: "))
+        numbers.append(num)
+        fin = input("enter done if finished, else anything")
+        if fin == "done":
+            add = False
+        else:
+            add = True
+    maxNum = numbers[0]
+    minNum = numbers[0]
+    for num in numbers:
+        if num > maxNum:
+            maxNum = num
+        else:
+            maxNum = maxNum
+        
+        if num < minNum:
+            minNum = num
+    
+    total = [maxNum,minNum]
+    return total
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
-
-
-if __name__ == "__main__":
-    main()
+values = find_min_max()
+print("your max and min values are: ", values)
