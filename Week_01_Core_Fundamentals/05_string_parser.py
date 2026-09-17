c851561e4db38b27518496b77ec20de44384ecee
LScarsbrook26 <48038@rws.uk.net>
2026-09-17 19:40:42 +0100

    05_string_parser.py
    Implemented word_split function to split a sentence into words and added demonstration code for user input.

--- Week_01_Core_Fundamentals/05_string_parser.py
+++ Week_01_Core_Fundamentals/05_string_parser.py
@@ -12,12 +12,24 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
 
+def word_split(sentence):
+    add = []
+    word = ""
+    for i in sentence:
+        if i == " ":
+            if word != "":
+                add.append(word)
+                word = ""
+        else:
+            word = word + i
 
-if __name__ == "__main__":
-    main()
+    if word != "":
+        add.append(word)
+
+    return add
+
+sentence = input("Enter a sentence: ")
+sep = word_split(sentence)
+print("number of words:", len(sep))
+print("list of words:", sep)
