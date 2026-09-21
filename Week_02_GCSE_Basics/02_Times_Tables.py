a965e83466b5a5c19633381bd1ae9520087159f5
LScarsbrook26 <48038@rws.uk.net>
2026-09-21 18:57:55 +0100

    02_Times_Tables.py
    Added a multiplier function to generate a times table and demonstration code for user input.

--- Week_02_GCSE_Basics/02_Times_Tables.py
+++ Week_02_GCSE_Basics/02_Times_Tables.py
@@ -11,12 +11,13 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
+def multiplier(num):
+   table = []
+   for i in range(1,13):
+       ans = i * num
+       table.append(ans)
+   return table
 
-
-if __name__ == "__main__":
-    main()
+num = int(input("enter number gor its times table, 1-12: "))
+cal = multiplier(num)
+print(cal)
