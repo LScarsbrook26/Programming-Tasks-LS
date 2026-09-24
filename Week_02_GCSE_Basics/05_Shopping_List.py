0ed5c37597c938371784dbe8a38832462f5e5696
LScarsbrook26 <48038@rws.uk.net>
2026-09-24 09:41:07 +0100

    05_Shopping_List.py
    Implemented a shopping list function that allows users to add items and edit them. Added demonstration code to run the function when the script is executed.

--- Week_02_GCSE_Basics/05_Shopping_List.py
+++ Week_02_GCSE_Basics/05_Shopping_List.py
@@ -11,12 +11,26 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
+def shopping_list():
+    shopping = []
+    done = True
+    while done == True:
+        add = input("add items to shopping list: (enter done if finished adding)")
+        shopping.append(add)
+        if add == "done":
+            print(shopping)
+            done = False
+        else:
+            done = True
+    edit = input("do u want to edit the list: yes or no ")
+    if edit == "yes":
+        num = int(input("enter which number item u want to change: "))
+        change = input("enter what u want to change it to ")
+        shopping[num - 1] = change
+    else: 
+        print('finished')
+    
+    return shopping
 
-
-if __name__ == "__main__":
-    main()
+paper = shopping_list()
+print("completed shopping list:", paper)
